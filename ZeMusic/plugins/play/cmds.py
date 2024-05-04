11b2a750eb26f5a1c289b256ea6187605886668a
Newwissam <mmdhkwr85@gmail.com>
2024-05-05 02:59:41 +0300

    Update cmds.py

--- ZeMusic/plugins/play/cmds.py
+++ ZeMusic/plugins/play/cmds.py
@@ -17,7 +17,7 @@ async def zdatsr(client: Client, message: Message):
     usrnam = usr.username
     await message.reply_photo(
         photo=START_IMG_URL,
-        caption=f"""<b>» مرحبـاً بك عـزيـزي </b> {message.from_user.mention} .\n\n<b>» استخـدم الازرار بالاسفـل 𝄞\n» لـ تصفـح اوامـر الميـوزك 🥁</b>""",
+        caption=f"""<b>» مرحبـاً بك عـزيـزي </b> {message.from_user.mention} .\n\n<b>» استخـدم الازرار بالاسفـل \n» لـ تصفـح اوامـر الميـوزك </b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
