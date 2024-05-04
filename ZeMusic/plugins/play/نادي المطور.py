53a77787c643426c540991c88c4bd515dfd41951
Newwissam <mmdhkwr85@gmail.com>
2024-05-04 22:35:48 +0300

    Update نادي المطور.py

--- ZeMusic/plugins/play/نادي المطور.py
+++ ZeMusic/plugins/play/نادي المطور.py
@@ -46,6 +46,6 @@ async def call_dev(client: Client, message: Message):
     # إنشاء زر "اونلاين"
     online_button = InlineKeyboardButton(mname, url=f"https://t.me/{musrnam}")
     
-    await message.reply_text(f"<b> ⌯ تم إرسال النداء إلى مطور البوت\n\n ⌯ Dᥱꪜ -› @{musrnam} .</b>",
+    await message.reply_text(f"<b>  تم إرسال النداء إلى مطور البوت\n\n⌯ Dev -› @{musrnam} .</b>",
                              disable_web_page_preview=True,
                              reply_markup=InlineKeyboardMarkup([[online_button]]))
