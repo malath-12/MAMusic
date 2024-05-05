cf9c1693139f0ce8cb2b958663ce878d1e38cfc9
Newwissam <mmdhkwr85@gmail.com>
2024-05-05 18:26:11 +0300

    Update دعوه.py

--- ZeMusic/plugins/play/دعوه.py
+++ ZeMusic/plugins/play/دعوه.py
@@ -20,7 +20,7 @@ async def brah2(_, msg):
 
 @app.on_message(filters.video_chat_members_invited)
 async def brah3(app :app, message:Message):
-           text = f"↞ قام {message.from_user.mention} \n↞ بدعوة :"
+           text = f"↞ قام الحلو {message.from_user.mention} \n↞ بدعوتك يا :"
            x = 0
            for user in message.video_chat_members_invited.users:
              try:
