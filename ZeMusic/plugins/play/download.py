fddd3a9abc5d0bd51d3afcae2dd24f6c96c191f5
Newwissam <mmdhkwr85@gmail.com>
2024-05-07 12:10:59 +0300

    Update download.py

--- ZeMusic/plugins/play/download.py
+++ ZeMusic/plugins/play/download.py
@@ -51,7 +51,7 @@ async def song_downloader(client, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"𖡃 @{app.username} "
+        rep = f"-›  @{app.username} "
         host = str(info_dict["uploader"])
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
