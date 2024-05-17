ac61dcd691955c2293f21dd3b0749d5dea78c7e3
Newwissam <mmdhkwr85@gmail.com>
2024-05-17 14:39:48 +0300

    Update sudoers.py

--- ZeMusic/plugins/sudo/sudoers.py
+++ ZeMusic/plugins/sudo/sudoers.py
@@ -10,7 +10,7 @@ from ZeMusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
-@app.on_message(filters.command(["رفع مطور"]) & filters.user(OWNER_ID))
+@app.on_message(filters.command(["اضف مطور"]) & filters.user(OWNER_ID))
 @language
 async def useradd(client, message: Message, _):
     if not message.reply_to_message:
@@ -44,7 +44,7 @@ async def userdel(client, message: Message, _):
         await message.reply(_["sudo_8"])
 
 
-@app.on_message(filters.command(["المطورين", "listsudo", "sudoers"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["مطورين", "listsudo", "sudoers"]) & ~BANNED_USERS)
 @language
 async def sudoers_list(client, message: Message, _):
     text = _["sudo_5"]
