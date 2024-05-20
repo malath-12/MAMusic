4477b693c60554a51ef82ce0eb045335a7c6f080
Newwissam <mmdhkwr85@gmail.com>
2024-05-20 16:56:08 +0300

    Update Speakers.py

--- ZeMusic/plugins/play/Speakers.py
+++ ZeMusic/plugins/play/Speakers.py
@@ -1,51 +1,34 @@
-import asyncio
 from pyrogram import filters, Client
 from ZeMusic import app
-from ZeMusic.utils.database import *
-from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError)
-from ZeMusic.core.call import Mody
+import asyncio
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from ZeMusic.core.call import Mody
+from ZeMusic.utils.database import *
+from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError)
 
-@app.on_message(filters.regex("^مين في الكول$|^من في الكول$|^مين في كول$"))
+@app.on_message(filters.regex("من في الكول"))
 async def strcall(client, message):
     assistant = await group_assistant(Mody,message.chat.id)
     try:
-        await assistant.join_group_call(message.chat.id, AudioPiped("./assets/call.mp3"), stream_type=StreamType().pulse_stream)
-        text="🔔 الاعضاء المتواجدين في الكول :\n\n"
+        await assistant.join_group_call(message.chat.id, AudioPiped("./strings/call.mp3"), stream_type=StreamType().pulse_stream)
+        text="الحلوين الي في المكالمه :\n\n"
         participants = await assistant.get_participants(message.chat.id)
         k =0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut=f"٭{participant.volume}٭يتحدث 🗣٭"
+                mut="يتحدث🗣"
             else:
-                mut="٭ساكت 🔕٭"
+                mut="ساكت🔕"
             user = await client.get_users(participant.user_id)
-            print(participant.user_id)
             k +=1
-            text +=f"{k}➤{user.mention}{mut}\n"
-        text += f"\nعددهم : {len(participants)}\n✔️"    
+            text +=f"{k}:{user.mention}↬{mut}\n"
+        text += f"\nعدد الاشخاص في المكالمة المرئية ↬ ⦗ {len(participants)} ⦘"    
         await message.reply(f"{text}")
         await asyncio.sleep(5)
         await assistant.leave_group_call(message.chat.id)
     except NoActiveGroupCall:
-        await message.reply(f"المكالمه مقفوله \n❌")
-    except AlreadyJoinedError:
-        text="🔔 الاعضاء المتواجدين في الكول :\n\n"
-        participants = await assistant.get_participants(message.chat.id)
-        k =0
-        for participant in participants:
-            info = participant
-            if info.muted == False:
-                mut=f"٭{participant.volume}٭يتحدث 🗣٭"
-            else:
-                mut="٭ساكت 🔕٭"
-            user = await client.get_users(participant.user_id)
-            print(participant.user_id)
-            k +=1
-            text +=f"{k}➤{user.mention}{mut}\n"
-        text += f"\nعددهم : {len(participants)}\n✔️"
-        await message.reply(f"{text}")
+        await message.reply(f"لم يتم العثور على دردشة فيديو نشطة.\nيرجى بدء دردشة فيديو في مجموعتك/قناتك والمحاولة مرة أخرى.")
     except TelegramServerError:
-        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام\n❌")
+        await message.reply(f"ارسل مرة اخرى يوجد خطأ في احد سيرفرات التليكرام")
