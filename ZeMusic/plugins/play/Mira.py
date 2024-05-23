9040d61fc24f0a919459ae625cb88289998a95aa
Newwissam <mmdhkwr85@gmail.com>
2024-05-23 14:29:38 +0300

    Update Mira.py

--- ZeMusic/plugins/play/Mira.py
+++ ZeMusic/plugins/play/Mira.py
@@ -2,7 +2,7 @@ import asyncio
 from pyrogram import Client, filters
 from strings import get_command
 from strings.filters import command
-from YukkiMusic.utils.decorators import AdminActual
+from ZeMusic.utils.decorators import AdminActual
 from pyrogram.types import (
     CallbackQuery,
     InlineKeyboardButton,
@@ -12,7 +12,7 @@ from pyrogram.types import (
     InputMediaPhoto,
     Message,
 )
-from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
 @app.on_message(filters.regex("^$"))
 async def khalid(client: Client, message: Message):
